e20e3d291583d19284b632553136105ceb326468
Beloved-vince <97411920+Beloved-vince@users.noreply.github.com>
2022-06-02 14:07:04 +0100

    Add files via upload

---
+++ Boom Learning Website/boomHomepage.js
@@ -0,0 +1,4 @@
+(function (w, d) { for (var i = 0, j = d.getElementsByTagName("ins"), k = j[i]; i < j.length; k = j[++i])
+{ if(k.className == "adPushupAds" && k.getAttribute("data-push") != "1")
+ { ((w.adpushup = w.adpushup || {}).control = (w.adpushup.control || [])).push(k); k.setAttribute("data-push", "1");} } })
+(window, document);
